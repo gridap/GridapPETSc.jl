ebc37fd4137ed512993602411e70a8774acf7b9d
victorsndvg <victorsv@gmail.com>
2020-01-28 08:59:42 +0100

    Enable femdriver.jl and remove symfemdriver.jl.
    Still pending:
      - Manage Sparsematrix format from AffineFEOperator to avoid conversions.

--- test/femdriver.jl
+++ test/femdriver.jl
@@ -1,47 +1,43 @@
 module FEMDriver
 
-# Commented waiting for new_master to support it
-# 
-# using Test
-# using Gridap.Geometry
-# using GridapPETSc
-# 
-# tol = 1e-10
-# 
-# GridapPETSc.init!(["-ksp_rtol","$tol"]) 
-# 
-# model = CartesianDiscreteModel(
-#   domain=(0,1,0,1,0,1), partition=(10,10,10))
-# 
-# fespace = FESpace(
-#   reffe=:Lagrangian, order=1, valuetype=Float64,
-#   conformity=:H1, model=model, diritags="boundary")
-# 
-# V = TestFESpace(fespace)
-# U = TrialFESpace(fespace,0.0)
-# 
-# trian = Triangulation(model)
-# quad = CellQuadrature(trian,degree=2)
-# 
-# t_Ω = AffineFETerm(
-#   (v,u) -> inner(∇(v),∇(u)),
-#   (v) -> inner(v, (x) -> x[1]*x[2] ),
-#   trian, quad)
-# 
-# op = LinearFEOperator(SparseMatrixCSR{0,PetscReal,PetscInt},V,U,t_Ω)
-# 
-# ls = PETScSolver()
-# solver = LinearFESolver(ls)
-# 
-# uh = solve(solver,op)
-# 
-# x = free_dofs(uh)
-# A = op.mat
-# b = op.vec
-# 
-# r = A*x - b
-# @test maximum(abs.(r)) < tol
-# 
-# GridapPETSc.finalize!()
+using Test
+using Gridap
+using GridapPETSc
+
+tol = 1e-10
+
+GridapPETSc.init!(["-ksp_rtol","$tol"]) 
+
+model = CartesianDiscreteModel((0,1,0,1,0,1), (10,10,10))
+
+V = TestFESpace(reffe=:Lagrangian, order=1, valuetype=Float64,
+  conformity=:H1, model=model, dirichlet_tags="boundary")
+
+U = TrialFESpace(V)
+
+trian = get_triangulation(model)
+quad = CellQuadrature(trian,2)
+
+t_Ω = AffineFETerm(
+  (v,u) -> inner(∇(v),∇(u)),
+  (v) -> inner(v, (x) -> x[1]*x[2] ),
+  trian, quad)
+
+#op = AffineFEOperator(SparseMatrixCSR{0,PetscReal,PetscInt},V,U,t_Ω)
+op = AffineFEOperator(V,U,t_Ω)
+
+ls = PETScSolver()
+solver = LinearFESolver(ls)
+
+uh = solve(solver,op)
+
+x = get_free_values(uh)
+A = op.op.matrix
+b = op.op.vector
+
+r = A*x - b
+@test maximum(abs.(r)) < tol
+
+GridapPETSc.finalize!()
 
 end #module
