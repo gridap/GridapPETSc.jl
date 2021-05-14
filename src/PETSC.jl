e50448a01af798c9f625734560095adbae6792c5
Francesc Verdugo <fverdugo@cimne.upc.edu>
2021-05-14 11:38:44 +0200

    Adding some KSP and PC methods

--- src/PETSC.jl
+++ src/PETSC.jl
@@ -161,6 +161,7 @@ end
 Vec() = Vec(Ptr{Cvoid}())
 Base.convert(::Type{Vec},p::Ptr{Cvoid}) = Vec(p)
 
+@wrapper(:VecCreateSeq,PetscErrorCode,(MPI.Comm,PetscInt,Ptr{Vec}),(comm,n,vec),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/Vec/VecCreateSeq.html")
 @wrapper(:VecCreateSeqWithArray,PetscErrorCode,(MPI.Comm,PetscInt,PetscInt,Ptr{PetscScalar},Ptr{Vec}),(comm,bs,n,array,vec),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/Vec/VecCreateSeqWithArray.html")
 @wrapper(:VecDestroy,PetscErrorCode,(Ptr{Vec},),(vec,),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/Vec/VecDestroy.html")
 @wrapper(:VecView,PetscErrorCode,(Vec,PetscViewer),(vec,viewer),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/Vec/VecView.html")
@@ -168,6 +169,7 @@ Base.convert(::Type{Vec},p::Ptr{Cvoid}) = Vec(p)
 @wrapper(:VecGetValues,PetscErrorCode,(Vec,PetscInt,Ptr{PetscInt},Ptr{PetscScalar}),(x,ni,ix,y),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/Vec/VecGetValues.html")
 @wrapper(:VecAssemblyBegin,PetscErrorCode,(Vec,),(vec,),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/Vec/VecAssemblyBegin.html")
 @wrapper(:VecAssemblyEnd,PetscErrorCode,(Vec,),(vec,),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/Vec/VecAssemblyEnd.html")
+@wrapper(:VecPlaceArray,PetscErrorCode,(Vec,Ptr{PetscScalar}),(vec,array),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/Vec/VecPlaceArray.html")
 
 # Matrix related functions
 
@@ -224,5 +226,153 @@ Base.convert(::Type{Mat},p::Ptr{Cvoid}) = Mat(p)
 @wrapper(:MatAssemblyEnd,PetscErrorCode,(Mat,MatAssemblyType),(mat,typ),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/Mat/MatAssemblyEnd.html")
 @wrapper(:MatGetSize,PetscErrorCode,(Mat,Ptr{PetscInt},Ptr{PetscInt}),(mat,m,n),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/Mat/MatGetSize.html")
 @wrapper(:MatEqual,PetscErrorCode,(Mat,Mat,Ptr{PetscBool}),(A,B,flg),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/Mat/MatEqual.html")
+
+# KSP and PC related things
+
+"""
+Julia alias for `KSPType` C type.
+
+See [PETSc manual](https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/KSP/KSPType.html).
+"""
+const KSPType = Cstring
+const KSPRICHARDSON = "richardson"
+const KSPCHEBYSHEV  = "chebyshev"
+const KSPCG         = "cg"
+const KSPGROPPCG    = "groppcg"
+const KSPPIPECG     = "pipecg"
+const KSPPIPECGRR   = "pipecgrr"
+const KSPPIPELCG    = "pipelcg"
+const KSPPIPEPRCG   = "pipeprcg"
+const KSPPIPECG2    = "pipecg2"
+const KSPCGNE       = "cgne"
+const KSPNASH       = "nash"
+const KSPSTCG       = "stcg"
+const KSPGLTR       = "gltr"
+const KSPFCG        = "fcg"
+const KSPPIPEFCG    = "pipefcg"
+const KSPGMRES      = "gmres"
+const KSPPIPEFGMRES = "pipefgmres"
+const KSPFGMRES     = "fgmres"
+const KSPLGMRES     = "lgmres"
+const KSPDGMRES     = "dgmres"
+const KSPPGMRES     = "pgmres"
+const KSPTCQMR      = "tcqmr"
+const KSPBCGS       = "bcgs"
+const KSPIBCGS      = "ibcgs"
+const KSPFBCGS      = "fbcgs"
+const KSPFBCGSR     = "fbcgsr"
+const KSPBCGSL      = "bcgsl"
+const KSPPIPEBCGS   = "pipebcgs"
+const KSPCGS        = "cgs"
+const KSPTFQMR      = "tfqmr"
+const KSPCR         = "cr"
+const KSPPIPECR     = "pipecr"
+const KSPLSQR       = "lsqr"
+const KSPPREONLY    = "preonly"
+const KSPQCG        = "qcg"
+const KSPBICG       = "bicg"
+const KSPMINRES     = "minres"
+const KSPSYMMLQ     = "symmlq"
+const KSPLCD        = "lcd"
+const KSPPYTHON     = "python"
+const KSPGCR        = "gcr"
+const KSPPIPEGCR    = "pipegcr"
+const KSPTSIRM      = "tsirm"
+const KSPCGLS       = "cgls"
+const KSPFETIDP     = "fetidp"
+const KSPHPDDM      = "hpddm"
+
+"""
+Julia alias for `PCType` C type.
+
+See [PETSc manual](https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/PC/PCType.html).
+"""
+const PCType = Cstring
+const PCNONE            = "none"
+const PCJACOBI          = "jacobi"
+const PCSOR             = "sor"
+const PCLU              = "lu"
+const PCSHELL           = "shell"
+const PCBJACOBI         = "bjacobi"
+const PCMG              = "mg"
+const PCEISENSTAT       = "eisenstat"
+const PCILU             = "ilu"
+const PCICC             = "icc"
+const PCASM             = "asm"
+const PCGASM            = "gasm"
+const PCKSP             = "ksp"
+const PCCOMPOSITE       = "composite"
+const PCREDUNDANT       = "redundant"
+const PCSPAI            = "spai"
+const PCNN              = "nn"
+const PCCHOLESKY        = "cholesky"
+const PCPBJACOBI        = "pbjacobi"
+const PCVPBJACOBI       = "vpbjacobi"
+const PCMAT             = "mat"
+const PCHYPRE           = "hypre"
+const PCPARMS           = "parms"
+const PCFIELDSPLIT      = "fieldsplit"
+const PCTFS             = "tfs"
+const PCML              = "ml"
+const PCGALERKIN        = "galerkin"
+const PCEXOTIC          = "exotic"
+const PCCP              = "cp"
+const PCBFBT            = "bfbt"
+const PCLSC             = "lsc"
+const PCPYTHON          = "python"
+const PCPFMG            = "pfmg"
+const PCSYSPFMG         = "syspfmg"
+const PCREDISTRIBUTE    = "redistribute"
+const PCSVD             = "svd"
+const PCGAMG            = "gamg"
+const PCCHOWILUVIENNACL = "chowiluviennacl"
+const PCROWSCALINGVIENNACL = "rowscalingviennacl"
+const PCSAVIENNACL      = "saviennacl"
+const PCBDDC            = "bddc"
+const PCKACZMARZ        = "kaczmarz"
+const PCTELESCOPE       = "telescope"
+const PCPATCH           = "patch"
+const PCLMVM            = "lmvm"
+const PCHMG             = "hmg"
+const PCDEFLATION       = "deflation"
+const PCHPDDM           = "hpddm"
+const PCHARA            = "hara"
+
+"""
+Julia alias for the `KSP` C type.
+
+See [PETSc manual](https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/KSP/KSP.html).
+"""
+struct KSP
+  ptr::Ptr{Cvoid}
+end
+KSP() = KSP(Ptr{Cvoid}())
+Base.convert(::Type{KSP},p::Ptr{Cvoid}) = KSP(p)
+
+"""
+Julia alias for the `PC` C type.
+
+See [PETSc manual](https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/PC/PC.html).
+"""
+struct PC
+  ptr::Ptr{Cvoid}
+end
+PC() = PC(Ptr{Cvoid}())
+Base.convert(::Type{PC},p::Ptr{Cvoid}) = PC(p)
+
+@wrapper(:KSPCreate,PetscErrorCode,(MPI.Comm,Ptr{KSP}),(comm,inksp),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/KSP/KSPCreate.html")
+@wrapper(:KSPDestroy,PetscErrorCode,(Ptr{KSP},),(ksp,),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/KSP/KSPDestroy.html")
+@wrapper(:KSPSetFromOptions,PetscErrorCode,(KSP,),(ksp,),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/KSP/KSPSetFromOptions.html")
+@wrapper(:KSPSetUp,PetscErrorCode,(KSP,),(ksp,),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/KSP/KSPSetUp.html")
+@wrapper(:KSPSetOperators,PetscErrorCode,(KSP,Mat,Mat),(ksp,Amat,Pmat),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/KSP/KSPSetOperators.html")
+@wrapper(:KSPSetTolerances,PetscErrorCode,(KSP,PetscReal,PetscReal,PetscReal,PetscInt),(ksp,rtol,abstol,dtol,maxits),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/KSP/KSPSetTolerances.html")
+@wrapper(:KSPSolve,PetscErrorCode,(KSP,Vec,Vec),(ksp,b,x),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/KSP/KSPSolve.html")
+@wrapper(:KSPSolveTranspose,PetscErrorCode,(KSP,Vec,Vec),(ksp,b,x),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/KSP/KSPSolveTranspose.html")
+@wrapper(:KSPGetIterationNumber,PetscErrorCode,(KSP,Ptr{PetscInt}),(ksp,its),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/KSP/KSPGetIterationNumber.html")
+@wrapper(:KSPView,PetscErrorCode,(KSP,PetscViewer),(ksp,viewer),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/KSP/KSPView.html")
+@wrapper(:KSPSetInitialGuessNonzero,PetscErrorCode,(KSP,PetscBool),(ksp,flg),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/KSP/KSPSetInitialGuessNonzero.html")
+@wrapper(:KSPSetType,PetscErrorCode,(KSP,KSPType),(ksp,typ),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/KSP/KSPSetType.html")
+@wrapper(:KSPGetPC,PetscErrorCode,(KSP,Ptr{PC}),(ksp,pc),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/KSP/KSPGetPC.html")
+@wrapper(:PCSetType,PetscErrorCode,(PC,PCType),(pc,typ),"https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/PC/PCSetType.html")
 
 end # module
--- test/PETSCTests.jl
+++ test/PETSCTests.jl
@@ -5,7 +5,7 @@ using MPI
 using SparseMatricesCSR
 using GridapPETSc.PETSC
 using GridapPETSc.PETSC: @check_error_code
-using GridapPETSc.PETSC: PetscBool, PetscInt, PetscScalar, Vec, Mat
+using GridapPETSc.PETSC: PetscBool, PetscInt, PetscScalar, Vec, Mat, KSP, PC
 
 if !MPI.Initialized()
     MPI.Init()
@@ -80,11 +80,11 @@ idxn = PetscInt[3,1]
 
 @check_error_code PETSC.MatDestroy(mat)
 
-I = PetscInt[1,3,4,2]
-J = PetscInt[3,1,1,5]
-V = PetscScalar[1,2,3,4]
+I = PetscInt[1,1,2,2,2,3,3,3,4,4]
+J = PetscInt[1,2,1,2,3,2,3,4,3,4]
+V = PetscScalar[4,-2,-1,6,-2,-1,6,-2,-1,4]
 m = PetscInt(4)
-n = PetscInt(5)
+n = PetscInt(4)
 A = sparsecsr(Val(0),I,J,V,m,n)
 
 i = A.rowptr
@@ -106,6 +106,40 @@ n2 = Ref{PetscInt}()
 @test m2[] == m
 @test n2[] == n
 
+x = ones(PetscScalar,m)
+b = A*x
+bt = transpose(A)*x
+x2 = similar(x)
+x2 .= 0
+
+ksp = Ref{KSP}()
+pc = Ref{PC}()
+
+rhs = Ref{Vec}()
+sol = Ref{Vec}()
+
+@check_error_code PETSC.VecCreateSeqWithArray(comm,1,m,b,rhs)
+@check_error_code PETSC.VecCreateSeqWithArray(comm,1,m,x2,sol)
+
+@check_error_code PETSC.KSPCreate(comm,ksp)
+@check_error_code PETSC.KSPSetOperators(ksp[],mat[],mat[])
+@check_error_code PETSC.KSPGetPC(ksp[],pc)
+@check_error_code PETSC.PCSetType(pc[],PETSC.PCJACOBI)
+@check_error_code PETSC.KSPSetFromOptions(ksp[])
+
+@check_error_code PETSC.KSPSolve(ksp[],rhs[],sol[])
+@test x ≈ x2
+
+@check_error_code PETSC.VecPlaceArray(rhs[],bt)
+
+@check_error_code PETSC.KSPSolveTranspose(ksp[],rhs[],sol[])
+@test x ≈ x2
+
+@check_error_code PETSC.KSPView(ksp[],C_NULL)
+
+@check_error_code PETSC.KSPDestroy(ksp)
+@check_error_code PETSC.VecDestroy(rhs)
+@check_error_code PETSC.VecDestroy(sol)
 @check_error_code PETSC.MatDestroy(mat)
 
 @check_error_code PETSC.PetscInitialized(flag)
