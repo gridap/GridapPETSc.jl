fe8fea08bfc0462199708d06547b42a61b02ab7c
Francesc Verdugo <fverdugo@cimne.upc.edu>
2021-05-18 09:01:23 +0200

    More testing in PoissonDriver

--- test/PoissonDriver.jl
+++ test/PoissonDriver.jl
@@ -2,6 +2,7 @@ module PoissonDriver
 
 using Test
 using Gridap
+using Gridap.FESpaces
 using GridapPETSc
 using GridapPETSc: PetscScalar, PetscInt
 using SparseArrays
@@ -35,7 +36,7 @@ GridapPETSc.with(args=options) do
   
   order = 1
   reffe = ReferenceFE(lagrangian,Float64,order)
-  V = TestFESpace(model,reffe,dirichlet_tags="boundary" )
+  V = TestFESpace(model,reffe,dirichlet_tags="boundary",vector_type=Vector{PetscScalar})
   U = TrialFESpace(V)
   Ω = Triangulation(model)
   dΩ = Measure(Ω,2*order)
@@ -48,41 +49,74 @@ GridapPETSc.with(args=options) do
   
   # Assembling on a Julia matrix 
   # with the same data layout as petsc
+  # (efficient use of PETScSolver)
   Tm = SparseMatrixCSR{0,PetscScalar,PetscInt}
   Tv = Vector{PetscScalar}
+  Tx = get_vector_type(U)
   assem = SparseMatrixAssembler(Tm,Tv,U,V)
   op = AffineFEOperator(a,l,U,V,assem)
   uh = solve(solver,op)
   x = get_free_dof_values(uh)
   A = get_matrix(op)
   b = get_vector(op)
+  @test typeof(A) == Tm
+  @test typeof(b) == Tv
+  @test typeof(x) == Tx
   r = A*x - b
   @test maximum(abs.(r)) < tol
 
   # Assembling on a Julia matrix 
   # with different data layout than petsc
+  # (inefficient use of PETScSolver)
   Tm = SparseMatrixCSC{Float64,Int}
   Tv = Vector{Float64}
+  Tx = get_vector_type(U)
   assem = SparseMatrixAssembler(Tm,Tv,U,V)
   op = AffineFEOperator(a,l,U,V,assem)
   uh = solve(solver,op)
   x = get_free_dof_values(uh)
   A = get_matrix(op)
   b = get_vector(op)
+  @test typeof(A) == Tm
+  @test typeof(b) == Tv
+  @test typeof(x) == Tx
   r = A*x - b
   @test maximum(abs.(r)) < tol
 
-  ## Now assemble on a native PETScMarix and PETScVector
-  #Tm = PETScMarix
-  #Tv = PETScVector
-  #assem = SparseMatrixAssembler(Tm,Tv,U,V)
-  #op = AffineFEOperator(a,l,U,V,assem)
-  #uh = solve(solver,op)
-  #x = get_free_dof_values(uh)
-  #A = get_matrix(op)
-  #b = get_vector(op)
-  #r = A*x - b
-  #@test maximum(abs.(r)) < tol
+  # Now assemble on a native PETScMarix but on a Julia Vector
+  # with same memory layout as PETScVector
+  # (efficient use of PETScSolver)
+  Tm = PETScMatrix
+  Tv = Vector{PetscScalar}
+  Tx = get_vector_type(U)
+  assem = SparseMatrixAssembler(Tm,Tv,U,V)
+  op = AffineFEOperator(a,l,U,V,assem)
+  uh = solve(solver,op)
+  x = get_free_dof_values(uh)
+  A = get_matrix(op)
+  b = get_vector(op)
+  @test typeof(A) == Tm
+  @test typeof(b) == Tv
+  @test typeof(x) == Tx
+  r = A*x - b
+  @test maximum(abs.(r)) < tol
+
+  # Now assemble on a native PETScMarix and on a native PETScVector
+  # (efficient use of PETScSolver)
+  Tm = PETScMatrix
+  Tv = PETScVector
+  Tx = get_vector_type(U)
+  assem = SparseMatrixAssembler(Tm,Tv,U,V)
+  op = AffineFEOperator(a,l,U,V,assem)
+  uh = solve(solver,op)
+  x = get_free_dof_values(uh)
+  A = get_matrix(op)
+  b = get_vector(op)
+  @test typeof(A) == Tm
+  @test typeof(b) == Tv
+  @test typeof(x) == Tx
+  r = A*x - b
+  @test maximum(abs.(r)) < tol
 
 end
 
