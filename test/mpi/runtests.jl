538f3d911b733c464f552f7262d38308d4a81a23
Javier Principe <principe@cimne.upc.edu>
2022-07-20 19:20:05 +0200

    Minor fixes to tests (see issue 83)

--- test/mpi/runtests.jl
+++ test/mpi/runtests.jl
@@ -1,5 +1,9 @@
 module GridapPETScMPITests
 using Test
+using MPI
+if MPI.Initialized()
+    MPI.Finalize()
+end
 @time @testset "PartitionedArrays" begin include("PartitionedArraysTestsRun.jl") end
 @time @testset "PLaplacianTests" begin include("PLaplacianTestsRun.jl") end
 @time @testset "GCTests" begin include("GCTestsRun.jl") end
